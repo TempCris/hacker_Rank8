ab41a6d33b61946482726584b9caa43af7a3eaa9
kross <cristemperatum@gmail.com>
2021-12-26 13:48:56 -0600

    ejercicio8

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 /* eslint-disable no-return-assign */
 
-// Source: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+// Source: https:https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
 
-function simpleArraySum(ar: Array<number>): number {
-  let suma = 0;
-  ar.forEach((element) => suma += element);
-  return suma;
+function birthdayCakeCandles(candles: number[]): number { /*se declara la funcion en donde se le pone que recibira datos de tipo numero*/
+    let tmax = 0;                                          /*se declara una variable max*/
+    let velaS = 0;                                         /*se declara una variable cont*/
+      candles.forEach((tvela) => {                         /*se declara un ciclo forEach*/      
+      if (tvela> tmax) {                                   /*se declara las condiciones que debe de cumplir el ciclo que item sea menor a max*/ 
+        tmax = tvela;                                       /*su condicion de asignacion cuando mx sea igual que item pasa a la siguiente linea*/
+        velaS = 1;                                         /*su objetivo de avance que es que cuando se logre la linea pasada cont sea uno*/
+      } else if (tvela === tmax) {                          /*si no se cumple el if sucedera ento entonces, su condicion es que cuando item sea igual a max pasa a la siguiente linea*/ 
+        velaS++;                                           /*que cont aumente uno*/
+      }                                                   /*finizila el for.each*/
+    });
+  return velaS;
 }
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const testArray = [1, 2, 3, 4, 10, 11];
-  const result = simpleArraySum(testArray);
+  const testArray = [3, 2, 1, 3];
+  const result = birthdayCakeCandles(testArray);
   console.log('Result: ', result);
 }
 
